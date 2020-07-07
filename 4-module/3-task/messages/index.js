1b60ea89df9cb11edbf6cbf93aa0dd519933ef8a
vladimirshameev <vladimir.shameev@gmail.com>
2020-07-07 23:52:28 +0300

    add solution for 4-module 3-task

---
+++ 4-module/3-task/messages/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ACCESS_DECLINED_MSG: 'Вложенные папки не поддерживаются',
+  NOT_FOUND_MSG: 'Файла на диске нет',
+  DELETE_FILE_SUCCESS_MSG: 'Файл удален',
+  UNEXPECTED_ERROR_MSG: 'Не известная ошибка',
+};
--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,16 +1,31 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {ACCESS_DECLINED_MSG, NOT_FOUND_MSG, DELETE_FILE_SUCCESS_MSG} = require('./messages');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end(ACCESS_DECLINED_MSG);
+      } else {
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end(NOT_FOUND_MSG);
+          } else {
+            res.statusCode = 200;
+            res.end(DELETE_FILE_SUCCESS_MSG);
+          }
+        });
+      }
 
       break;
 
